95034da1f492bd9843f94d64f75cd877f04459a0
PaulManRobertson <bigwallpaul514@gmail.com>
2023-04-17 13:31:51 -0600

    finished portfolio:

--- src/components/hooks/useThemeSwitcher.js
+++ src/components/hooks/useThemeSwitcher.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 const useThemeSwitcher = () => {
+  
   const preferDarkQuery = "(prefer-color-scheme: dark)";
   const [mode, setMode] = useState("");
 
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -10,6 +10,7 @@ import artcile3 from "../../public/images/articles/create modal component in rea
 import artcile4 from "../../public/images/articles/form validation in reactjs using custom react hook.png";
 import artcile5 from "../../public/images/articles/smooth scrolling in reactjs.png";
 import { motion, useMotionValue } from "framer-motion";
+import TransitionEffect from "@/components/TransitionEffect";
 
 const FramerImage = motion(Image);
 
@@ -47,7 +48,7 @@ const MovingImage = ({ title, img, link }) => {
         ref={imgRef}
         src={img}
         alt={title}
-        className="z-10 w-96 h-auto hidden absolute rounded-lg"
+        className="z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden"
       />
     </Link>
   );
@@ -59,10 +60,12 @@ const Article = ({ img, title, date, link }) => {
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.75, ease: "easeInOut" } }}
       viewport={{ once: true }}
-      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-motion.ght text-dark first:mt-0 border border-somotion.d border-dark gorder-r-4 border-b-4 dark:border-light dark:bg-dark"
+      className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-motion.ght text-dark first:mt-0 border border-somotion.d border-dark gorder-r-4 border-b-4 
+        dark:border-light dark:bg-dark dark:text-light
+        sm:flex-col"
     >
       <MovingImage title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4 dark:text-primaryDark">
+      <span className="text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm">
         {date}
       </span>
     </motion.li>
@@ -71,7 +74,7 @@ const Article = ({ img, title, date, link }) => {
 
 const FeaturedArticle = ({ img, title, time, summary, link }) => {
   return (
-    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl">
+    <li className="relative col-span-1 w-full p-4 bg-light border border-solid border-dark rounded-2xl dark:bg-dark dark:border-light">
       <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] bg-dark rounded-br-3xl" />
 
       <Link
@@ -85,15 +88,21 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
           className="w-full h-auto"
           whileHover={{ scale: 1.05 }}
           transition={{ duration: 0.2 }}
+          priority
+          sizes="(max-width: 768px) 100vw,
+            (max-width: 1200px) 50vw,
+            50vw"
         />
       </Link>
       <Link href={link} target="_blank">
-        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline">
+        <h2 className="capitalize text-2xl font-bold my-2 mt-4 hover:underline xs:text-lg">
           {title}
         </h2>
       </Link>
       <p className="text-sm mb-2">{summary}</p>
-      <span className="text-primary font-semibold">{time}</span>
+      <span className="text-primary font-semibold dark:text-primaryDark">
+        {time}
+      </span>
     </li>
   );
 };
@@ -105,13 +114,14 @@ const articles = () => {
         <title>BIGWALLPAUL | About Page</title>
         <meta name="description" content="any description" />
       </Head>
+      <TransitionEffect />
       <main className="2-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light">
         <Layout className="pt-16">
           <AnimatedText
             text="Hello! I am Paul RObertson and this is an article"
-            className="mb-16"
+            className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"
           />
-          <ul className="grid grid-cols-2 gap-16">
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <FeaturedArticle
               title="Build A Custom Pagination Component In Reactjs From Scratch"
               summary="Learn how to build a custom pagination component in ReactJS from scratch. Follow this step-by-step guide to integrate Pagination component in your ReactJS project."
